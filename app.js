8a2ac54aa29c4465fe02ee4f5991920db534b749
Aditya Matsa <aditya.matsa@icloud.com>
2024-06-13 11:51:37 +0530

    Import dinosaur data and create dinosaur objects

--- app.js
+++ app.js
@@ -1,3 +1,80 @@
+const data = {
+  Dinos: [
+    {
+      species: "Triceratops",
+      weight: 13000,
+      height: 114,
+      diet: "herbavor",
+      where: "North America",
+      when: "Late Cretaceous",
+      fact: "First discovered in 1889 by Othniel Charles Marsh",
+    },
+    {
+      species: "Tyrannosaurus Rex",
+      weight: 11905,
+      height: 144,
+      diet: "carnivor",
+      where: "North America",
+      when: "Late Cretaceous",
+      fact: "The largest known skull measures in at 5 feet long.",
+    },
+    {
+      species: "Anklyosaurus",
+      weight: 10500,
+      height: 55,
+      diet: "herbavor",
+      where: "North America",
+      when: "Late Cretaceous",
+      fact: "Anklyosaurus survived for approximately 135 million years.",
+    },
+    {
+      species: "Brachiosaurus",
+      weight: 70000,
+      height: "372",
+      diet: "herbavor",
+      where: "North America",
+      when: "Late Jurasic",
+      fact: "An asteroid was named 9954 Brachiosaurus in 1991.",
+    },
+    {
+      species: "Stegosaurus",
+      weight: 11600,
+      height: 79,
+      diet: "herbavor",
+      where: "North America, Europe, Asia",
+      when: "Late Jurasic to Early Cretaceous",
+      fact: "The Stegosaurus had between 17 and 22 seperate places and flat spines.",
+    },
+    {
+      species: "Elasmosaurus",
+      weight: 16000,
+      height: 59,
+      diet: "carnivor",
+      where: "North America",
+      when: "Late Cretaceous",
+      fact: "Elasmosaurus was a marine reptile first discovered in Kansas.",
+    },
+    {
+      species: "Pteranodon",
+      weight: 44,
+      height: 20,
+      diet: "carnivor",
+      where: "North America",
+      when: "Late Cretaceous",
+      fact: "Actually a flying reptile, the Pteranodon is not a dinosaur.",
+    },
+    {
+      species: "Pigeon",
+      weight: 0.5,
+      height: 9,
+      diet: "herbavor",
+      where: "World Wide",
+      when: "Holocene",
+      fact: "All birds are living dinosaurs.",
+    },
+  ],
+};
+
 // Create Dino Constructor
 function Dino(dino) {
   this.species = dino.species;
@@ -10,6 +87,15 @@ function Dino(dino) {
 }
 
 // Create Dino Objects
+
+const dino0 = new Dino(data.Dinos[0]);
+const dino1 = new Dino(data.Dinos[1]);
+const dino2 = new Dino(data.Dinos[2]);
+const dino3 = new Dino(data.Dinos[3]);
+const dino4 = new Dino(data.Dinos[4]);
+const dino5 = new Dino(data.Dinos[5]);
+const dino6 = new Dino(data.Dinos[6]);
+const dino7 = new Dino(data.Dinos[7]);
 
 // Create Human Object
 
